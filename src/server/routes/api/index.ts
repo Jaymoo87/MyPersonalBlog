536415439079792073af206a67b8a72e3d8b5500
Justin Murrah <jdmurrah@gmail.com>
2023-01-08 14:22:00 -0600

    organized the routes a abit

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 
 const router = Router();
 
-router.use("/user");
+router.use("/authors");
 
 export default router;
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,18 +2,17 @@ import * as express from "express";
 import blogRouter from "./blogs";
 import donateRouter from "./donate";
 import tagRouter from "./tags";
-import contactRouter from './contact'
+import contactRouter from "./contact";
+import apiRouter from "./api";
+import authRouter from "./auth";
 
+const indexRouter = express.Router();
 
-
-const indexRouter = express.Router()
-
-
-indexRouter.use("/api/blogs", blogRouter)
-indexRouter.use("/api/tags", tagRouter)
-indexRouter.use("/api/donate", donateRouter)
-indexRouter.use("/api/contact", contactRouter)
-
-
+indexRouter.use("/api/blogs", blogRouter);
+indexRouter.use("/api/tags", tagRouter);
+indexRouter.use("/api/donate", donateRouter);
+indexRouter.use("/api/contact", contactRouter);
+indexRouter.use("/api", apiRouter);
+indexRouter.use("/auth", authRouter);
 
 export default indexRouter;
